71aa99ac801bd58a6241fa78537f5b74afd54d3f
Silvio Paganini <s2paganini@gmail.com>
2015-10-26 10:32:19 +0100

    dynamic gallery

--- src/view/Gallery.js
+++ src/view/Gallery.js
@@ -1,9 +1,18 @@
 import Wallop from "wallop";
 
 class Gallery  {
-  constructor(args) {
-    let wallop = new Wallop(document.querySelector('.Wallop'));
+  constructor() {
 
+    let list = document.querySelector('.Wallop-list');
+
+    for (var i = 0; i < window.PHOTOS.length; i++) {
+        let div = document.createElement('div');
+        div.className = 'Wallop-item'
+        div.style.backgroundImage = 'url("static/gallery/'+window.PHOTOS[i]+'")'
+        list.appendChild(div);
+    };
+
+    let wallop = new Wallop(document.querySelector('.Wallop'));
   }
 }
 
